0a61cc3076c9eea11a038471c893a102443904d8
thephantompig791 <thephantompig791@gmail.com>
2022-09-10 12:54:28 -0400

    Textured the rice cakes

--- src/main/java/net/thephantompig791/ricecakes/items/ModItems.java
+++ src/main/java/net/thephantompig791/ricecakes/items/ModItems.java
@@ -1,6 +1,7 @@
 package net.thephantompig791.ricecakes.items;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -10,7 +11,7 @@ import net.thephantompig791.ricecakes.Ricecakes;
 public class ModItems {
 
     public static final Item RICE_CAKE = registerItem("rice_cake",
-            new Item(new FabricItemSettings().group(ItemGroup.FOOD)));
+            new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(8).saturationModifier(3f).snack().build())));
 
 
     private static Item registerItem(String name, Item item) {
